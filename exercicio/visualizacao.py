3159f189bc3496cbce10ec8eaca25567636ead74
tecStudent <pedroms.pms@gmail.com>
2024-02-27 22:29:13 -0300

    first exercise - variables

---
+++ exercicio/visualizacao.py
@@ -0,0 +1,16 @@
+import csv
+from sys import argv
+
+import pandas as pd
+import seaborn as sns
+
+# Extraindo as colunas hora e taxa
+def Vizualizacao(nome_imagem):
+  df = pd.read_csv('./taxa-cdi.csv')
+
+  # Salvando no grafico
+
+  grafico = sns.lineplot(x=df['hora'], y=df['taxa'])
+  _ = grafico.set_xticklabels(labels=df['hora'], rotation=90)
+  grafico.get_figure().savefig(f'{nome_imagem}.png')
+  
